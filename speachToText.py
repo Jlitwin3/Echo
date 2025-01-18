9336be5193d9342568d395ec152eca6e7c8ca711
Jlitwin3 <161399314+Jlitwin3@users.noreply.github.com>
2025-01-18 11:34:10 -0800

    Add files via upload

---
+++ speachToText.py
@@ -0,0 +1,27 @@
+import pyaudio
+import wave
+
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+frames = []
+
+try: 
+    while True: 
+        data = stream.read(1024)
+        frames.append(data)
+except KeyboardInterrupt: 
+    pass
+
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+soundfile = wave.open("myrecording.wav", "wb")
+soundfile.setnchannels(1)
+soundfile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+soundfile.setframerate(44100)
+soundfile.writeframes(b''.join(frames))
+soundfile.close()
